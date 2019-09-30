e713ab7acfdd19ff3e687a5075cec0f2f9fab41d
andreitch <andreitch.top@gmail.com>
2019-09-30 17:24:33 +0300

    axios loading agent

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from "axios";
+import { IActivity } from "../models/activity";
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const sleep = (ms: number) => (response: AxiosResponse) =>
+  new Promise<AxiosResponse>(resolve =>
+    setTimeout(() => resolve(response), ms)
+  );
+
+const request = {
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
+};
+
+const Activities = {
+  list: (): Promise<IActivity[]> => request.get("/activities"),
+  details: (id: string) => request.get(`/activities/${id}`),
+  create: (activity: IActivity) => request.post("/activities", activity),
+  update: (activity: IActivity) =>
+    request.put(`/activities/${activity.id}`, activity),
+  delete: (id: string) => request.del(`/activities/${id}`)
+};
+
+export default {
+  Activities
+};
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -93,7 +95,7 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
