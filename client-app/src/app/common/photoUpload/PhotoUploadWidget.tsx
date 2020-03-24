dd211103d0eab033dee15dcc5702f19cc0a74094
andreitch <andreitch.top@gmail.com>
2020-03-24 19:23:33 +0200

    Photo Upload Widget with react-dropzone

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -1,23 +1,36 @@
-import React, { Fragment } from "react";
-import { Header, Grid } from "semantic-ui-react";
+import React, { Fragment, useState, useEffect } from "react";
+import { Header, Grid, Image } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
+import PhotoWdgetDropzone from "./PhotoWidgetDropzone";
 
-const PhotoUploadWidget = () => (
-  <Fragment>
-    <Grid>
-      <Grid.Column width={4}>
-        <Header color="teal" sub content="Step 1 - Add Photo" />
-      </Grid.Column>
-      <Grid.Column width={1} />
-      <Grid.Column width={4}>
-        <Header sub color="teal" content="Step 2 - Resize image" />
-      </Grid.Column>
-      <Grid.Column width={1} />
-      <Grid.Column width={4}>
-        <Header sub color="teal" content="Step 3 - Preview & Upload" />
-      </Grid.Column>
-    </Grid>
-  </Fragment>
-);
+const PhotoUploadWidget = () => {
+  const [files, setFiles] = useState<any[]>([]);
+
+  useEffect(() => {
+    return () => {
+      files.forEach(file => URL.revokeObjectURL(file.preview));
+    };
+  });
+
+  return (
+    <Fragment>
+      <Grid>
+        <Grid.Column width={4}>
+          <Header color="teal" sub content="Step 1 - Add Photo" />
+          <PhotoWdgetDropzone setFiles={setFiles} />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 2 - Resize image" />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 3 - Preview & Upload" />
+          {files.length > 0 && <Image src={files[0].preview} />}
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  );
+};
 
 export default observer(PhotoUploadWidget);
