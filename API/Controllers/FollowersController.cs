06428642f14772029c354d9c8f2a1a308fca5987
andreitch <andreitch.top@gmail.com>
2020-04-07 14:55:06 +0300

    Getting a List of Followings for a user

--- API/Controllers/FollowersController.cs
+++ API/Controllers/FollowersController.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Followers;
+using Application.Profiles;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +20,12 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Unfollow(string username)
         {
             return await Mediator.Send(new Delete.Command{Username=username});
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowings(string username, string predicate)
+        {
+            return await Mediator.Send(new List.Query{Username = username, Predicate = predicate});
         }
     }
 }
