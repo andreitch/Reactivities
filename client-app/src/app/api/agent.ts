cc95756588c43304788a418af1b59483b1e1430c
andreitch <andreitch.top@gmail.com>
2020-04-07 15:51:01 +0300

    Adding thFollow Unfollow methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -74,7 +74,9 @@ const Profiles = {
   uploadPhoto: (photo: Blob): Promise<IPhoto> => request.postForm(`/photos`, photo),
   setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
   deletePhoto: (id:string) => request.del(`/photos/${id}`),
-  updateProfile: (profile: Partial<IProfile>) => request.put(`/profiles`, profile)
+  updateProfile: (profile: Partial<IProfile>) => request.put(`/profiles`, profile),
+  follow: (username: string) => request.post(`/profiles/${username}/follow`, {}),
+  unfollow: (username: string) => request.del(`/profiles/${username}/follow`)
 }
 
 export default {
--- client-app/src/app/stores/ProfileStore.ts
+++ client-app/src/app/stores/ProfileStore.ts
@@ -111,4 +111,38 @@ export default class ProfileStore {
       toast.error('Problem updating profile')
     }
   }
+
+  @action follow = async (username: string) => {
+    this.loading = true;
+    try {
+      await agent.Profiles.follow(username);
+      runInAction(() => {
+        this.profile!.following = true;
+        this.profile!.followersCount++;
+        this.loading = false;
+      })
+    } catch (error) {
+      toast.error("Problem following user");
+      runInAction(() => {
+        this.loading = false;
+      })
+    }
+  }
+
+  @action unfollow = async (username: string) => {
+    this.loading = true;
+    try {
+      await agent.Profiles.unfollow(username);
+      runInAction(() => {
+        this.profile!.following = false;
+        this.profile!.followersCount--;
+        this.loading = false;
+      })
+    } catch (error) {
+      toast.error("Problem unfollowing user");
+      runInAction(() => {
+        this.loading = false;
+      })
+    }
+  }
 }
