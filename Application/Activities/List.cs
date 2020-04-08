301b5f5b212fe4812ec86441f52b0897f4b6649b
andreitch <andreitch.top@gmail.com>
2020-04-08 15:57:41 +0300

    Paging Activity List

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -9,26 +10,49 @@ using Persistence;
 
 namespace Application.Activities
 {
-  public class List
-  {
-    public class Query : IRequest<List<ActivityDto>> { }
-
-    public class Handler : IRequestHandler<Query, List<ActivityDto>>
+    public class List
     {
-      private readonly DataContext _context;
-      private readonly IMapper _mapper;
-      public Handler(DataContext context, IMapper mapper)
-      {
-        _mapper = mapper;
-        _context = context;
-      }
+        public class ActivitiesEnvelope
+        {
+            public List<ActivityDto> Activities { get; set; }
+            public int ActivityCount { get; set; }
+        }
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            public Query(int? limit, int? offset)
+            {
+                Limit = limit;
+                Offset = offset;
 
-      public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
-      {
-        var activities = await _context.Activities
-            .ToListAsync();
-        return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
-      }
+            }
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var queryable = _context.Activities.AsQueryable();
+
+                var activities = await queryable
+                  .Skip(request.Offset ?? 0)
+                  .Take(request.Limit ?? 3).ToListAsync();
+
+                return new ActivitiesEnvelope
+                {
+                  Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                  ActivityCount = queryable.Count()
+                };
+            }
+        }
     }
-  }
 }
