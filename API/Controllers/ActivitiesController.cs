53797086baa34c1c8750b51c07303d95aa9ab9a2
andreitch <andreitch.top@gmail.com>
2020-04-09 06:38:24 +0300

    Filtering Activity List in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,9 +11,9 @@ namespace API.Controllers
     public class ActivitiesController : BaseController
     {
         [HttpGet]
-        public async Task<ActionResult<ActivitiesEnvelope>> List(int? limit, int? offset)
+        public async Task<ActionResult<ActivitiesEnvelope>> List(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
         {
-            return await Mediator.Send(new List.Query(limit, offset));
+            return await Mediator.Send(new List.Query(limit, offset, isGoing, isHost, startDate));
         }
 
         [HttpGet("{id}")]
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -19,29 +21,53 @@ namespace Application.Activities
         }
         public class Query : IRequest<ActivitiesEnvelope>
         {
-            public Query(int? limit, int? offset)
+            private readonly bool _isGoing;
+            private readonly bool _isHost;
+            private readonly DateTime? _startDate;
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
+                StartDate = startDate ?? DateTime.Now;
+                IsHost = isHost;
+                IsGoing = isGoing;
                 Limit = limit;
                 Offset = offset;
 
             }
             public int? Limit { get; set; }
             public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime StartDate { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
 
             public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var queryable = _context.Activities.AsQueryable();
+                var queryable = _context.Activities
+                    .Where(x => x.Date >= request.StartDate)
+                    .OrderBy(x => x.Date)
+                    .AsQueryable();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
+
+                if (request.IsHost && !request.IsGoing)
+                {
+                    queryable = queryable.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
 
                 var activities = await queryable
                   .Skip(request.Offset ?? 0)
@@ -49,8 +75,8 @@ namespace Application.Activities
 
                 return new ActivitiesEnvelope
                 {
-                  Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
-                  ActivityCount = queryable.Count()
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                    ActivityCount = queryable.Count()
                 };
             }
         }
